d55df26d271ef95c44f85592670e4663d06e316f
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2023-03-25 15:09:18 +0530

    Imported springboot initializer file to begin with

---
+++ src/main/java/com/blog/myblogapp/MyblogappApplication.java
@@ -0,0 +1,13 @@
+package com.blog.myblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogappApplication.class, args);
+	}
+
+}
